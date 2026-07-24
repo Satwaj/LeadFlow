3c1de22ca03aa8bf0530a9fbb9cdb5bef5549e4b
satwaj <satwajbachhav2004@gmail.com>
2026-07-25 03:46:28 +0530

    restoreSession

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import { restoreSession } from "./redux/slices/authSlice.js";
 
-const App = () => <AppRoutes />;
+const App = () => {
+  const dispatch = useDispatch();
+  const { sessionChecked } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    if (!sessionChecked) {
+      dispatch(restoreSession());
+    }
+  }, [dispatch, sessionChecked]);
+
+  return <AppRoutes />;
+};
 
 export default App;
